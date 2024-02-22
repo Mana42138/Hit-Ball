71d01b12a0f8e62d958f93a8ff43a92c83163491
Mana <86923626+Mana42138@users.noreply.github.com>
2024-02-22 16:49:56 +0100

    Update Instances.lua

--- StarterGui/Manager/Instances.lua
+++ StarterGui/Manager/Instances.lua
@@ -1,78 +1,52 @@
-return function()
-	local TweenService = game:GetService("TweenService")
-	local Instances = require(script.Parent.Instances)
-	
-	local Grad = Instances.Standard.UIGradient
-	local frames = 50
-	local speed = 0.01
+local Table = {}
 
-	local function colour_change(color, keypoint)
-		local originalColour = Grad.Color.Keypoints[keypoint + 1].Value
-		local sequence
-		for i = frames, 0, -1 do
-			task.wait(speed)
+local TweenService = game:GetService("TweenService")
 
-			local x = math.sqrt((i - frames) ^ 2) / frames
+local Player = game.Players.LocalPlayer
 
-			if keypoint == 0 then
-				sequence = ColorSequence.new {
-					ColorSequenceKeypoint.new(0, originalColour:Lerp(color, x)),
-					ColorSequenceKeypoint.new(1, Grad.Color.Keypoints[2].Value)
-				}
-			else
-				sequence = ColorSequence.new {
-					ColorSequenceKeypoint.new(0, Grad.Color.Keypoints[1].Value),
-					ColorSequenceKeypoint.new(1, originalColour:Lerp(color, x))
-				}
-			end
-
-			Grad.Color = sequence
-		end
-	end
-
-	spawn(function()
-		while true do
-			colour_change(Color3.fromRGB(255, math.random(70, 200), 33), 0)
-			colour_change(Color3.fromRGB(255, math.random(70, 200), 15), 1)
-			task.wait(math.random(.3, .5))
-		end
-	end)
-	
-	local Grad = Instances.Pro.UIGradient
-	local frames = 50
-	local speed = 0.01
-
-	local function colour_change2(color, keypoint)
-		local originalColour = Grad.Color.Keypoints[keypoint + 1].Value
-		local sequence
-		for i = frames, 0, -1 do
-			task.wait(speed)
-
-			local x = math.sqrt((i - frames) ^ 2) / frames
-
-			if keypoint == 0 then
-				sequence = ColorSequence.new {
-					ColorSequenceKeypoint.new(0, originalColour:Lerp(color, x)),
-					ColorSequenceKeypoint.new(1, Grad.Color.Keypoints[2].Value)
-				}
-			else
-				sequence = ColorSequence.new {
-					ColorSequenceKeypoint.new(0, Grad.Color.Keypoints[1].Value),
-					ColorSequenceKeypoint.new(1, originalColour:Lerp(color, x))
-				}
-			end
-
-			Grad.Color = sequence
-		end
-	end
-	
-	spawn(function()
-		while true do
-			colour_change2(Color3.fromRGB(73, math.random(116, 150), math.random(195, 255)), 0)
-			colour_change2(Color3.fromRGB(73, math.random(116, 150), math.random(195, 255)), 1)
-			task.wait(math.random(.3, .5))
-		end
+local function Animate(object)
+	object.MouseEnter:Connect(function()
+		TweenService:Create(
+			object.UIScale,
+			TweenInfo.new(.3, Enum.EasingStyle.Quad),
+			{Scale = 1.05}
+		):Play()
 	end)
 
+	object.MouseLeave:Connect(function()
+		TweenService:Create(
+			object.UIScale,
+			TweenInfo.new(.3, Enum.EasingStyle.Quad),
+			{Scale = 1}
+		):Play()
+	end)
 	
+	object.MouseButton1Click:Connect(function()
+		TweenService:Create(
+			object.UIScale,
+			TweenInfo.new(.3, Enum.EasingStyle.Quad),
+			{Scale = 0.88}
+		):Play()
+		task.wait(.1)
+		TweenService:Create(
+			object.UIScale,
+			TweenInfo.new(.3, Enum.EasingStyle.Quad),
+			{Scale = 1}
+		):Play()
+	end)
 end
+
+for i,v in pairs(Player.PlayerGui:GetDescendants()) do
+	Table[v.Name] = v
+	if v:IsA("TextButton") then
+		if not v:FindFirstChildOfClass("UIScale") then
+			-- Animate All buttons!
+			local ui_scale = Instance.new("UIScale")
+			ui_scale.Parent = v
+			ui_scale.Scale = 1
+			Animate(v)
+		end
+	end
+end
+
+return Table
